7cc538b0125ee5cf92148778334ad03b18b327a6
John Davi Dutra Canuto Pires <jddcp@ic.ufal.br>
2021-05-26 21:15:32 -0300

    Update Ford.cpp

--- Ford.cpp
+++ Ford.cpp
@@ -11,54 +11,54 @@ using namespace std;
 // Retorna True se existe caminho de s a t no grafo residual
 bool BFS(vector<vector<int>>& res_graph, int s, int t, int parent[])
 {
-	// cria um array de vértices visitados
+	// Cria um array de vértices visitados
 	bool visit_vextex[V];
 	memset(visit_vextex, 0, sizeof(visit_vextex));
-	queue<int> q; //cria fila
-	q.push(s); // enfileira o vértice inicial
-	visit_vextex[s] = true; // marca como visitado
+	queue<int> q; // Cria fila
+	q.push(s); // Enfileira o vértice inicial
+	visit_vextex[s] = true; // Marca como visitado
 	parent[s] = -1;
 
-	while (!q.empty()) { // enquanto a fila não estiver vazia, u recebe o primeiro elemento da fila
+	while (!q.empty()) { // Enquanto a fila não estiver vazia, u recebe o primeiro elemento da fila
 		int u = q.front();
 		q.pop();
 
 		for (int v = 0; v < V; v++) {
 			if (visit_vextex[v] == false && res_graph[u][v] > 0) {
-				if (v == t) { // se atingiu o destino t
-					parent[v] = u; // adiciona u como nó pai de v no caminho
-					return true; // existe um caminho de s a t
+				if (v == t) { // Se atingiu o destino t
+					parent[v] = u; // Adiciona u como nó pai de v no caminho
+					return true; // Existe um caminho de s a t
 				}
 				q.push(v); 
-				parent[v] = u; // guarda o caminho a cada passo
-				visit_vextex[v] = true; // marca o vértice como visitado
+				parent[v] = u; // Guarda o caminho a cada passo
+				visit_vextex[v] = true; // Marca o vértice como visitado
 			}
 		}
 	}
 
-	return false; // destino t não atingido
+	return false; // Destino t não atingido
 }
 
 int ford_fulkerson(vector<vector<int>>& graph, int s, int t)
 {
 	int u, v;
-	vector<vector<int>>& res_graph = graph; // grafo de capacidades residuais de i a j para cada elemento res_graph[i][j]
-    int parent[V]; // array de caminhos de s a t
-	int f_max = 0; // fluxo inicial
+	vector<vector<int>>& res_graph = graph; // Grafo de capacidades residuais de i a j para cada elemento res_graph[i][j]
+    int parent[V]; // Array de caminhos de s a t
+	int f_max = 0; // Fluxo inicial
 
 	for (u = 0; u < V; u++)
 		for (v = 0; v < V; v++)
 			res_graph[u][v] = graph[u][v];
 
-	// busca e atualização do fluxo até o máximo enquanto houver caminho de s a t
+	// Busca e atualização do fluxo até o máximo enquanto houver caminho de s a t
 	while (BFS(res_graph, s, t, parent)) { 
 		int f_path = INT_MAX;
 		for (v = t; v != s; v = parent[v]) {
 			u = parent[v];
-			f_path = min(f_path, res_graph[u][v]); // capacidade residual mínima ao longo de um dado caminho dado por 'parent'
+			f_path = min(f_path, res_graph[u][v]); // Capacidade residual mínima ao longo de um dado caminho dado por 'parent'
 		}
 
-        // capacidades residuais atualizadas ao longo do caminho
+        // Capacidades residuais atualizadas ao longo do caminho
 		for (v = t; v != s; v = parent[v]) {
 			u = parent[v];
 			res_graph[u][v] = res_graph[u][v] - f_path;
